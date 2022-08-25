b1ff2ca6df195d2265b6b420164b9f6ec2a3307b
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-08-25 17:40:43 -0500

    terminado

--- faker.js
+++ faker.js
@@ -1,4 +1,21 @@
 const { faker } = require('@faker-js/faker');
-const randomName = faker.name.fullName(); // Rowan Nikolaus
-console.log(`empleado ${randomName}`);
+const fs = require('fs');
+
+function generateUsers(){
+    let users = []
+    for(let i = 0;i<1000;i++){
+        const randomName = faker.name.fullName();
+        users.push(randomName);
+    }
+    return users
+}
+const generateData = generateUsers();
+fs.writeFileSync(`names.txt`,generateData.join(" ") );
+if(generateData.length === 1000){
+    console.log("El proceso ha terminado satisfactoriamente")
+}else{
+    console.log("A ocurrido un error")
+}
+
+
 
